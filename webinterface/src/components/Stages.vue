c2aa32efea7131cfddaea9f88c77d0fed9edd3ec
AW Shulzycki <alexshulzycki@gmail.com>
2025-08-03 15:53:29 +0200

    renamed stages vue page

---
+++ webinterface/src/components/Stages.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {useStageStore} from "@/stores/StageStore.ts";
+const StageInterface = useStageStore();
+
+</script>
+
+<template>
+<button @click="StageInterface.syncServerStageInformation()">Sync stage status from server</button>
+  <button @click="StageInterface.updateStageInfo()">Tell the server to refresh</button>
+
+  <li v-for="[id, val] in StageInterface.serverStages">
+    <h4>{{val.identifier}}</h4>
+    <h4>{{val.model}}</h4>
+    <p v-if="val.connected">Connected</p><p v-else> Disconnected</p>
+    <p v-if="val.ready">Ready</p> <p v-else>Not Ready</p>
+    <p>{{val.kind}}</p>
+    <p>{{val.minimum}} - {{val.maximum}}mm</p>
+    <p>Position: {{val.position}}</p>
+    <p v-if="val.ontarget">On target</p> <p v-else> Not on target</p>
+  </li>
+
+</template>
+
+<style scoped>
+
+</style>
--- webinterface/src/router.ts
+++ webinterface/src/router.ts
@@ -2,13 +2,13 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 
 import Main from "./Main.vue"
 import Controllers from "./Controllers.vue"
-import StageStatus from "@/components/stageStatus.vue";
+import Stages from "@/components/Stages.vue";
 import Assembly3D from "@/components/3D/Assembly3D.vue";
 
 const routes = [
   { path: '/', component: Main },
   { path: '/controllers', component: Controllers },
-  { path: '/stages', component:StageStatus},
+  { path: '/stages', component:Stages},
   { path: '/Assembly3D', component: Assembly3D },
 ]
 
