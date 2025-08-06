79c8b017b22a44c20380559a98b3b084778a70f4
AW Shulzycki <alexshulzycki@gmail.com>
2025-08-06 20:16:48 +0200

    nested window grids now window grid

--- webinterface/src/components/Layout/WindowGrid.vue
+++ webinterface/src/components/Layout/WindowGrid.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 
 
-import {type WindowGrid, WindowGridPayloads} from "@/stores/LayoutStore.ts";
+import {type WindowGrid, WindowGridOrientation, WindowGridPayloads} from "@/stores/LayoutStore.ts";
+import {watch, ref} from "vue";
 
 const {windowgrid} = defineProps<{ windowgrid: WindowGrid }>();
 
+const WGprop1 = ref<WindowGrid>({
+  orientation: WindowGridOrientation.horizontal,
+  components: [["empty", {}],["empty", {}]]
+})
+
+const WGprop2 = ref<WindowGrid>({
+  orientation: WindowGridOrientation.horizontal,
+  components: [["empty", {}],["empty", {}]]
+})
 
 function saveLayout() {
   window.alert('implement pls')
@@ -18,6 +28,14 @@ function removeSelf(which: number) {
   windowgrid.components[which] = ["empty", {}]
 }
 
+function getwindowgrid(){
+  return windowgrid
+}
+
+watch(getwindowgrid, (newValue) => {
+  console.log("change", newValue)
+})
+
 </script>
 
 <template>
@@ -29,16 +47,18 @@ function removeSelf(which: number) {
   <div class="windowgrid">
     <div v-if="windowgrid.components[0][0] != 'empty'" class="one">
       <button @click="removeSelf(0)">Remove</button>
-      <component v-bind:is="windowgrid.components[0][0]" v-model="windowgrid.components[0][1]"/>
+      <component v-if="windowgrid.components[0][0] != 'WindowGrid'" v-bind:is="windowgrid.components[0][0]" v-model="windowgrid.components[0][1]"/>
+      <WindowGrid v-else-if="windowgrid.components[0][0] == 'WindowGrid'" v-model:windowgrid="WGprop1"/>
     </div>
     <div v-else>
       <select v-model="windowgrid.components[0][0]">
-        <option v-for="op in WindowGridPayloads" value="op">{{op}}</option>
+        <option v-for="op in WindowGridPayloads" v-bind:value="op">{{op}}</option>
       </select>
     </div>
     <div v-if="windowgrid.components[1][0] != 'empty'" class="two">
       <button @click="removeSelf(1)">Remove</button>
-      <component v-bind:is="windowgrid.components[1][0]" v-model="windowgrid.components[0][1]"/>
+      <component v-if="windowgrid.components[1][0] != 'WindowGrid'" v-bind:is="windowgrid.components[1][0]" v-model="windowgrid.components[0][1]"/>
+      <WindowGrid v-else-if="windowgrid.components[1][0] == 'WindowGrid'" v-model:windowgrid="WGprop2"/>
     </div>
     <div v-else>
       <select v-model="windowgrid.components[1][0]">
