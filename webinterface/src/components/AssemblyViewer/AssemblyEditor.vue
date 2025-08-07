63310ef6ceb472f60346265e3c9b804866969211
AW Shulzycki <alexshulzycki@gmail.com>
2025-08-07 15:10:20 +0200

    assembly editor now emits, ready for windowgrid

--- webinterface/src/components/AssemblyViewer/AssemblyEditor.vue
+++ webinterface/src/components/AssemblyViewer/AssemblyEditor.vue
@@ -2,16 +2,27 @@
 
 import {useAssemblyStore, ComponentType} from "@/stores/AssemblyStore.ts";
 import ChildViewer from "@/components/AssemblyViewer/ChildViewer.vue";
-const astore = useAssemblyStore()
+import {ref, watch} from "vue";
+import type {WindowGrid} from "@/stores/LayoutStore.ts";
 
+const astore = useAssemblyStore()
 astore.syncServerAssembly()
+
+const {root = "root"} = defineProps<{ root?:string }>();
+
+const rootnode = ref(root)
+const emit = defineEmits(['changetree'])
+
+watch(rootnode, ()=>{
+  emit("changetree", {root: rootnode.value})
+})
 
 </script>
 
 <template>
   <button v-if="astore.hasUnsavedEdits" @click="astore.submitEditAssembly()">Submit Edits</button>
   <h2>Root</h2>
-  <ChildViewer this_comp_name="root" />
+  <ChildViewer v-bind:this_comp_name="rootnode" />
 
 </template>
 
