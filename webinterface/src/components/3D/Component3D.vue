f237c50b33af3f3a281cc30e14784ad6a40329e8
AW Shulzycki <alexshulzycki@gmail.com>
2025-08-12 19:22:04 +0200

    axis line visualization now works thanks to just using three js directly

--- webinterface/src/components/3D/Component3D.vue
+++ webinterface/src/components/3D/Component3D.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 import {useLoop, useTresContext} from '@tresjs/core'
-import {Billboard, Box, Html, Sphere, Text3D} from '@tresjs/cientos';
+import {Billboard, Box, Html, Sphere, Line2} from '@tresjs/cientos';
 import {gsap} from "gsap"
-import {Quaternion, type QuaternionTuple, Vector3, type Vector3Tuple} from "three";
+import {
+  Quaternion,
+  type QuaternionTuple,
+  Vector3,
+  type Vector3Tuple,
+  Line,
+  BufferGeometry,
+  LineBasicMaterial
+} from "three";
 import {ref, shallowRef} from "vue";
 import {useAssemblyStore} from "@/stores/AssemblyStore.ts";
+import {useStageStore} from "@/stores/StageStore.ts";
 
 const {name, rot, vec, container} = defineProps<{
   name: string,
@@ -14,12 +23,30 @@ const {name, rot, vec, container} = defineProps<{
 }>();
 
 const astore = useAssemblyStore()
+const stagestore = useStageStore()
 
 const context = useTresContext()
 
 const posRef = shallowRef()
+const axisLine = shallowRef()
+
+
+const comp = ref()
+comp.value = astore.getEditMap.get(name)
+
+const axisValue = ref(0)
 
 const {onBeforeRender} = useLoop()
+
+const points = [];
+points.push(new Vector3(0, 0, 0));
+points.push(new Vector3(0, 0, 0));
+
+// We are drawing a line representing the axis manually since tresjs is dogshit
+const material = new LineBasicMaterial({color: 0x0000ff});
+const geometry = new BufferGeometry().setFromPoints(points);
+const line = new Line(geometry, material);
+context.scene.value.add(line)
 
 onBeforeRender(({delta}) => {
   // Compute difference in positions
@@ -29,11 +56,19 @@ onBeforeRender(({delta}) => {
   // TODO Fix GSAP quaternions
   gsap.to(posRef.value.position, new Vector3(vec[0], vec[1], vec[2]))
   gsap.to(posRef.value.rotation, new Quaternion(rot[0], rot[1], rot[2], rot[3]))
+
+
+  if (comp.value.axis_identifier != undefined) {
+    if (stagestore.serverStages.get(comp.value.axis_identifier)?.position != undefined) {
+      console.log(stagestore.serverStages.get(comp.value.axis_identifier)?.position)
+      const points = []
+      points.push(new Vector3().fromArray(vec));
+      points.push(new Vector3().fromArray(vec).add(new Vector3().fromArray(comp.value.axis_vector).multiplyScalar(stagestore.serverStages.get(comp.value.axis_identifier).position).applyQuaternion(new Quaternion().fromArray(rot))));
+      line.geometry = new BufferGeometry().setFromPoints(points)
+    }
+  }
 })
 
-
-const comp = ref()
-comp.value = astore.getEditMap.get(name)
 
 </script>
 
