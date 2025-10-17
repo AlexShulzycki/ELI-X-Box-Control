83a77700805a0badcc4511828ea3413a5d571d59
AW Shulzycki <alexshulzycki@gmail.com>
2025-10-17 14:18:45 +0200

    stage card functionality and css fixes

--- webinterface/src/components/Stages/Stage.vue
+++ webinterface/src/components/Stages/Stage.vue
@@ -3,40 +3,44 @@
 import {type FullState, type moveStageResponse, useStageStore} from "@/stores/StageStore.ts";
 import {computed, ref} from "vue";
 
-const {id} = defineProps<{ id?:number }>()
+const {id} = defineProps<{ id?: number }>()
 const emit = defineEmits(['changetree'])
 
 const stageStore = useStageStore()
 let res = ref<moveStageResponse>({success: true})
 const idselect = ref(id)
 const offset = ref(1)
+const absoluteInput = ref(0)
 
-function moveBy(offset: number){
-  if(idselect.value == undefined){return}
-  stageStore.stepStage(idselect.value, offset).then((value)=>{
-    if(value != undefined){
-      res.value = value
-    }
-  })
-}
-function moveTo(target: number){
-  if(idselect.value == undefined){return}
-  stageStore.moveStage(idselect.value, target).then((value)=>{
-    if(value != undefined){
+function moveBy(offset: number) {
+  if (idselect.value == undefined) {
+    return
+  }
+  stageStore.stepStage(idselect.value, offset).then((value) => {
+    if (value != undefined) {
       res.value = value
     }
   })
 }
 
-function processResponse(res: moveStageResponse){
-  if(!res.success){
+function moveTo() {
+  if (idselect.value == undefined || absoluteInput.value == undefined) return
+  stageStore.moveStage(idselect.value, absoluteInput.value).then((value) => {
+    if (value != undefined) {
+      res.value = value
+    }
+  })
+}
+
+function processResponse(res: moveStageResponse) {
+  if (!res.success) {
     window.alert(res.error)
   }
 }
 
-const state = computed(()=>{
+const state = computed(() => {
 
-  if(idselect.value != undefined){
+  if (idselect.value != undefined) {
     return stageStore.serverStages.get(idselect.value)
   }
 })
@@ -44,32 +48,39 @@ const state = computed(()=>{
 </script>
 
 <template>
-  <v-card v-if="state !== undefined"  width="auto" max-width="25%" text-no-wrap>
+  <v-card v-if="state !== undefined" width="auto" max-width="25%" text-no-wrap>
     <v-card-item v-if="!res.success">
-      Error: {{res.error}}
+      Error: {{ res.error }}
     </v-card-item>
-    <v-card-title>ID: {{state.identifier}}</v-card-title>
-    <v-card-text>Connected: {{state.connected}}</v-card-text>
-    <v-card-text>Ready: {{state.ready}}</v-card-text>
-    <v-card-text>Type: {{state.kind}}</v-card-text>
-    <v-card-text>Min-Max: {{state.minimum}}mm - {{state.maximum}}mm</v-card-text>
-    <v-card-text><b>On Target? {{state.ontarget}}</b></v-card-text>
-    <v-card-item>
-        <v-row>
-          <v-col></v-col>
-          <v-col><h6>Position</h6></v-col>
-          <v-col></v-col>
-        </v-row>
-        <v-row>
-          <v-col><v-btn @click="moveBy(-offset)" icon="mdi-minus"/></v-col>
-          <v-col>{{state.position}}</v-col>
-          <v-col><v-btn @click="moveBy(offset)" icon="mdi-plus"/></v-col>
-        </v-row>
+    <v-card-title>ID: {{ state.identifier }}</v-card-title>
+    <v-card-text>
+      {{ state.connected?"Connected":"Disconnected" }} <br>
+      {{ state.ready?"Ready":"Not ready" }} <br>
+      Type: {{ state.kind }} <br>
+      Range from {{ state.minimum }} to {{ state.maximum }} mm <br>
+      {{ state.ontarget?"On target" : "Not on target" }}
+    </v-card-text>
+    <v-card-actions>
 
-    </v-card-item>
+        Position
+          <v-btn @click="moveBy(-offset)" icon="mdi-minus"/>
+
+        {{ String(Math.round(state.position*100)/100) + "mm"}}
+
+          <v-btn @click="moveBy(offset)" icon="mdi-plus"/>
+    </v-card-actions>
+    <v-card-actions>
+      <br>
+
+      <v-number-input v-model="absoluteInput" precision="3" control-variant="stacked"/>
+      <v-btn @click="moveTo">Set</v-btn>
+
+    </v-card-actions>
   </v-card>
   <div v-else>
-    ID: <select v-model="idselect" @change="$emit('changetree', {id: idselect})"> <option v-for="ident in stageStore.getStageIDs">{{ident}}</option> </select>
+    ID: <select v-model="idselect" @change="$emit('changetree', {id: idselect})">
+    <option v-for="ident in stageStore.getStageIDs">{{ ident }}</option>
+  </select>
   </div>
 </template>
 
